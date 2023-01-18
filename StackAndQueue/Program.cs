c258323c9eaf1f3e08722cb93845889ea43fe4b8
Anisha0906 <anishadas880@gmail.com>
2023-01-18 23:19:09 +0530

    [Anisha]Added:Ability to create a stack of 56,30,70

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("Stack Structure- ");
+            stack.Display();
+            Console.ReadLine();
+        }
+    }
+}
